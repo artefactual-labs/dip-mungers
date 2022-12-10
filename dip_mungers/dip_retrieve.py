43b76c33fed569507afa6255c0bb9a7747f46370
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 04:12:09 +0000

    Adding tarfile member sanitization to extractall()

--- dip_mungers/dip_retrieve.py
+++ dip_mungers/dip_retrieve.py
@@ -120,7 +120,26 @@ def download_dip(amclient, dip_uuid, dip_basename):
 
     # Extract DIP from tarball.
     with tarfile.open(local_dip_path_tar) as dip_tar:
-        dip_tar.extractall(DESKTOP_PATH)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(dip_tar, DESKTOP_PATH)
 
     if not os.path.exists(local_dip_path):
         print("Error: Unable to extract DIP from tarball")
